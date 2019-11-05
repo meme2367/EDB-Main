f52d2b9f840a92f58ebfa15c3dc857871df434a8
Baek Junha <astoru@naver.com>
2019-11-05 19:15:56 +0900

    repository 구성

---
+++ app/src/main/java/org/edb/main/Boot.java
@@ -0,0 +1,25 @@
+package org.edb.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * test class
+ *
+ */
+public class Boot {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println('x');
+        Application application=new Application() {
+            @Override
+            public void start(Stage stage) throws Exception {
+
+            }
+        };
+    }
+
+}
---
+++ app/src/test/java/org/edb/TestClass.java
@@ -0,0 +1,10 @@
+package org.edb;
+
+import org.junit.Test;
+
+public class TestClass {
+    @Test
+    public void test() {
+        System.out.println("a");
+    }
+}
