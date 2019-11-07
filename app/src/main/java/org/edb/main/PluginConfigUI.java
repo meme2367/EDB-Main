13669bfa199c669e9980496dcf8845221d934025
Baek Junha <astoru@naver.com>
2019-11-07 15:15:10 +0900

    Issue006 : added UI package, move UI classes to UI package, renaming some classes and files

--- app/src/main/java/org/edb/main/PluginConfigUI.java
+++
@@ -1,4 +0,0 @@
-package org.edb.main;
-
-public class PluginConfigUI {
-}
--- app/src/main/java/org/edb/main/UI/BootApp.java
+++ app/src/main/java/org/edb/main/UI/BootApp.java
@@ -1,18 +1,13 @@
-package org.edb.main;
+package org.edb.main.UI;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.SplitPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.edb.main.Authorization;
 
-import java.io.IOException;
-
-public class MainUI extends Application {
+public class BootApp extends Application {
 
 
 
@@ -26,7 +21,7 @@ public class MainUI extends Application {
 
         if(token == ""){
             //token이 없으면
-            Parent root = FXMLLoader.load(getClass().getResource("../../../fxml/sceneMain.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/mainUI.fxml"));
 
             Scene scene = new Scene(root);
 
@@ -35,7 +30,7 @@ public class MainUI extends Application {
         }else{
 
             //token이 있으면
-            Parent root = FXMLLoader.load(getClass().getResource("../../../fxml/sceneMainAfterLogin.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/mainUIAfterLogin.fxml"));
 
             Scene scene = new Scene(root);
 
